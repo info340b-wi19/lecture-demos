ac22ca035749c40ae09e2816abf6818cc1240ede
Tim Carlson <timca@dashplatform.com>
2019-02-19 11:42:36 -0800

    playing with fetch

--- ajax/js/index.js
+++ ajax/js/index.js
@@ -1,8 +1,20 @@
 'use strict';
 
 
-function renderdata(data){
-    console.log(data)
+function renderDataList(data){
+    let list = document.querySelector('ol');
+    list.innerHTML = ''; //empty out old
+    data.forEach((item) => {
+        list.appendChild(renderDataItem(item));
+    });
+}
+
+function renderDataItem(data) {
+
+    let li = document.createElement('li');
+    li.textContent = data.name;
+    console.log(data);
+    return li;
 }
  
 let form = document.querySelector('form');                      // grab the form element
@@ -17,6 +29,36 @@ form.addEventListener('submit', function(event) {               // listen for wh
 })
     .then(function(data){
     // console.log(data);
-    renderdata(data);
+    renderDataList(data);
 })
 })
+
+
+// function renderTaskItem(task) {
+//     let li = document.createElement('li');
+//     li.textContent = task.description;
+//     if(task.complete) {
+//       li.classList.add('font-strike');
+//     }
+//     //add remove callback
+//     li.addEventListener('click', function() {
+//       task.complete = !task.complete;
+//       renderTaskList();
+//     });
+  
+//     return li;
+//   }
+ 
+  
+//   function renderTaskList(){
+//     let list = document.querySelector('ol');
+//     list.innerHTML = ''; //empty out old
+  
+//     state.taskList.forEach((task) => {  //element for each
+//       list.appendChild(renderTaskItem(task)); //add the the list!
+//     });
+   
+//     //show button status
+//     renderInput();
+//     // renderButtonAbility();  
+//   }
